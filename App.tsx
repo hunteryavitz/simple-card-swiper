42b0858054e6f85bd36b4d3ac509a98a7444ea4d
hunteryavitz <hunteryavitz@protonmail.com>
2024-02-11 15:37:30 -0700

    init: initial commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, View } from 'react-native';
+import Ball from './src/Ball';
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <Ball />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});

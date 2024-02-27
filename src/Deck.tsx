ff0fae8d4b4f3ec3bf217624d5d60d336ac86f36
hunteryavitz <hunteryavitz@protonmail.com>
2024-02-26 19:00:31 -0700

    wip: add swipe animation to card

--- src/Deck.tsx
+++ src/Deck.tsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import {PanResponder, View, Animated} from "react-native";
+import {PanResponder, View, Animated, Dimensions} from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get('window').width
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH
+const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component {
 
@@ -14,15 +18,76 @@ class Deck extends Component {
             onPanResponderMove: (event, gestureState) => {
                 position.setValue({ x: gestureState.dx, y: gestureState.dy });
             },
-            onPanResponderRelease: () => {}
+            onPanResponderRelease: (event, gestureState) => {
+                if (gestureState.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe(1)
+                } else if (gestureState.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe(0)
+                } else {
+                    this.resetPosition();
+                }
+            }
 
         })
         this.state = { panResponder, position };
     }
 
+    forceSwipe(direction: number) {
+        const x = direction === 0 ? -SCREEN_WIDTH : SCREEN_WIDTH
+        // @ts-ignore
+        Animated.timing(this.state.position, {
+            toValue: {x, y: 0},
+            useNativeDriver: false,
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction))
+    }
+
+    onSwipeComplete(direction: number) {
+        const { onSwipeLeft, onSwipeRight, data } = this.props
+        const item = data[this.state.index]
+        direction === 0 ? onSwipeLeft(item) : onSwipeRight(item)
+        // @ts-ignore
+        this.state.position.setValue({ x: 0, y: 0 })
+        // @ts-ignore
+        this.setState({ index: this.state.index + 1 })
+    }
+
+    resetPosition() {
+        // @ts-ignore
+        Animated.spring(this.state.position, {
+            toValue: { x: 0, y: 0 },
+            useNativeDriver: false
+        }).start();
+    }
+
+    getCardStyle() {
+        // @ts-ignore
+        const { position } = this.state;
+
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            // @ts-ignore
+            ...this.state.position.getLayout(),
+            transform: [{ rotate }]
+        }
+    }
+
     renderCards() {
         // @ts-ignore
-        return this.props.data.map((item: any) => {
+        return this.props.data.map((item: any, index: number) => {
+            if (index === 0) {
+                return (
+                    // @ts-ignore
+                    <Animated.View key={item.id} style={this.getCardStyle()} {...this.state.panResponder.panHandlers}>
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
+
             // @ts-ignore
             return this.props.renderCard(item);
         });
@@ -30,13 +95,9 @@ class Deck extends Component {
 
     render() {
         return (
-            <Animated.View
-                // @ts-ignore
-                style={this.state.position.getLayout()}
-                // @ts-ignore
-                {...this.state.panResponder.panHandlers}>
+            <View>
                 {this.renderCards()}
-            </Animated.View>
+            </View>
         )
     }
 }
